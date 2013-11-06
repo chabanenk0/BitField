5a1b3382f290f25bf1e2c27dfe02fa28059aa7e7
avirta <vodoleeva11@mail.ru>
2013-11-06 09:51:54 +0200

    Update BitField.cpp

--- BitField.cpp
+++ BitField.cpp
@@ -11,6 +11,7 @@ class BitField
   BitField(long newData);
   BitField(const BitField &x);
   ~BitField();
+
   long getData();
   void setData(long newData);
   friend BitField operator&(BitField & a, BitField & b); //and
@@ -22,6 +23,43 @@ class BitField
   friend istream& operator>>(istream& os, BitField & a); //read from string 
   int getBit(int BitNumber);
   int setBit(int BitNumber, int bit);
+  char getBit (int bitNumber);
 }
 
 // test the dev branch
+
+char BitField::getBit (int bitNumber)
+{
+  long z = data>>bitNumber;
+  return z&1;
+}  
+
+BitField::BitField(long newData)
+
+{  data=newData;
+}
+
+BitField::BitField(const BitField &x)
+{
+  data=x.data;
+
+}
+
+BitField::BitField()
+{
+  data=0;
+}
+
+BitField::~BitField()
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+  BitField b1;
+  BitField b2(256);
+  BitField b3=b2;
+  system("PAUSE");
+  return EXIT_SUCCESS;
+}
